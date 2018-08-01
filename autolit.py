e5170646341c97738c7ef5215ce59a8006d78a3b
songdoyouho <34295885+songdoyouho@users.noreply.github.com>
2018-08-01 14:45:52 +0800

    fix some lines and add some interaction

--- autolit.py
+++ autolit.py
@@ -37,19 +37,16 @@ def check_liked_or_not(flaggg):
     #frame = cv2.cvtColor(heart, cv2.COLOR_BGR2RGB)
     #cv2.imshow("screen box", frame)
     heart_size = heart.shape
-    #print(heart_size[0])
     #cv2.waitKey(0)
     counter = 0
-    #thr = int(heart_size[0] * heart_size[1])
     for i in range(heart_size[0]):
         for j in range(heart_size[1]):
             tmp1=heart[i, j, 0]
             tmp2=heart[i, j, 1]
             tmp3=heart[i, j, 2]
-            #print(tmp1,tmp2,tmp3)
             if tmp1 == 254 and tmp2 == 55 and tmp3 == 155:
                 counter += 1
-    #print(counter)
+
     if counter > 100:
         flaggg = True # 有按過愛心
 
@@ -66,7 +63,6 @@ def check_end_or_not(flaggg):
     #frame = cv2.cvtColor(heart, cv2.COLOR_BGR2RGB)
     #cv2.imshow("screen box", frame)
     heart_size = heart.shape
-    #print(heart_size[0])
     #cv2.waitKey(0)
     counter = 0
     thr = int(heart_size[0]*heart_size[1])
@@ -75,13 +71,12 @@ def check_end_or_not(flaggg):
             tmp1=heart[i, j, 0]
             tmp2=heart[i, j, 1]
             tmp3=heart[i, j, 2]
-            #print(tmp1,tmp2,tmp3)
             if tmp1 < 100 and tmp2 < 100 and tmp3 < 100:
                 counter += 1
-    #print(counter)
+
     if counter > (thr - 30):
         flaggg = False
-    #print(flaggg)
+
     return flaggg
 
 def click_first_video():    
@@ -103,7 +98,6 @@ def add_friends_init():
     pyautogui.moveTo(left_x + int((box_x*0.03614+box_x*0.13253)/2) , left_y + int((box_y*0.61699+box_y*0.67409)/2) )
     pyautogui.click()
     time.sleep(1)
-    #display_img(545, 44)
     pyautogui.moveTo(left_x + int(box_x/2) , left_y + int((box_y*0.93732+box_y)/2) )
     pyautogui.click()
 
@@ -129,20 +123,16 @@ def add_friends():
     pyautogui.click()
 
 def check_dead_or_not():
-    #tstart = time.time()
     img = ImageGrab.grab(bbox = (left_x, left_y, right_x, right_y))
     img_np = np.array(img)
     #frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
     #cv2.imshow("screen box", frame)
-    # count 0
     count = 0
     img_size = img_np.shape
     for i in range(img_size[0]):
         for j in range(img_size[1]):
             if img_np[i, j, 0] == 0 and img_np[i, j, 1] == 0 and img_np[i, j, 2] == 0:
                 count += 1
-    #tend= time.time()
-    #print(tend - tstart, count, img_size[0]*img_size[1])
 
     if count > img_size[0]*img_size[1]/4*3:
         click_next_video()
@@ -150,10 +140,10 @@ def check_dead_or_not():
     else:
         pass
 
-def send_some_words():
+def send_some_words(input_send_message):
     pyautogui.moveTo((left_x + right_x)/2 , left_y + int((box_y*0.95403+box_y*0.97493)/2) ) # 愛心
     pyautogui.click()
-    pyautogui.typewrite('liked! please like my posts! Thanks!!!')
+    pyautogui.typewrite(input_send_message)
     pyautogui.moveTo(left_x + int((box_x*0.8265+box_x*0.86506)/2) , left_y + int((box_y*0.95403+box_y*0.97493)/2) ) # send
     pyautogui.click()
 
@@ -172,6 +162,17 @@ while main_flag:
         add_friends()
 
     elif input_order == str(2): # like the posts and send message
+        input_send_number = input("你想要傳多少次訊息：")
+        input_post_number = input("你想要隔幾篇限時動態傳一次訊息：")
+        input_send_message = input("你想要傳什麼訊息：")
+
+        # check if user want to use default or not
+        if len(input_post_number) == 0 and len(input_send_number) == 0 and len(input_send_message) == 0:
+            print("use default settings!")
+            input_send_number = 15
+            input_post_number = 50
+            input_send_message = "Liked! Please like my posts! Thanks!"
+
         click_first_video()
         flag = True
         post_counter = 0
@@ -193,21 +194,33 @@ while main_flag:
                 flag = check_end_or_not(flag)
 
             post_counter += 1
-            if post_counter == 15:
-                send_some_words()
+            if post_counter == int(input_post_number):
+                send_some_words(input_send_message)
                 send_counter += 1
                 post_counter = 0
             
-            if send_counter == 50: # to avoid be banned
+            if send_counter == int(input_send_number): # to avoid be banned
                 break
 
     elif input_order == str(3): # add friends and like the posts
+        input_send_number = input("你想要傳多少次訊息：")
+        input_post_number = input("你想要隔幾篇限時動態傳一次訊息：")
+        input_send_message = input("你想要傳什麼訊息：")
+
+        # check if user want to use default or not
+        if len(input_post_number) == 0 and len(input_send_number) == 0 and len(input_send_message) == 0:
+            print("use default settings!")
+            input_send_number = 15
+            input_post_number = 50
+            input_send_message = "Liked! Please like my posts! Thanks!"
+
         add_friends_init()
         add_friends()
         time.sleep(2)
         click_first_video()
         flag = True
         post_counter = 0
+        send_counter = 0
         while flag:
             like = None
             like = check_liked_or_not(like)
@@ -225,9 +238,13 @@ while main_flag:
                 flag = check_end_or_not(flag)
 
             post_counter += 1
-            if post_counter == 15:
-                send_some_words()
+            if post_counter == int(input_post_number):
+                send_some_words(input_send_message)
+                send_counter += 1
                 post_counter = 0
+
+            if send_counter == int(input_send_number): # to avoid be banned
+                break
 
     elif input_order == str(4):
         add_friends_init()
